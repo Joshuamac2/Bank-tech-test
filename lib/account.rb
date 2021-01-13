483fc089d19ff2accc0c09e3c8fbcf5c3a05d299
Joshua <joshuamacleod@live.com>
2021-01-13 19:36:13 +0000

    User can deposit funds

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,15 @@
 require_relative 'transaction'
 
 class Account
-  attr_reader :transactions
+  attr_reader :balance, :transactions
 
   def initialize
+    @balance = 0
     @transactions = []
+  end
+
+  def deposit(amount)
+    @balance += amount
+    @transactions << Transaction.new(amount)
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,9 +1,21 @@
 require 'account'
 
 describe Account do
-  describe '#transactions' do
-    it 'has 0 transactions in history' do
+
+  describe '#initialize' do
+    it 'balance starts nil' do
       expect(subject.transactions).to be_empty
+    end
+
+    it 'transactions in history empty' do
+      expect(subject.balance).to eq 0
+    end
+  end
+
+  describe '#deposit' do
+    it "increases the balance by deposited amount" do
+      subject.deposit(100)
+      expect(subject.balance).to eq 100
     end
   end
 end
